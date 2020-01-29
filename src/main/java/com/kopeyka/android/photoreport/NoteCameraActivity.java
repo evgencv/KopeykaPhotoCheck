15ab41f7cfefe6353f7332c7b7faa54537c5cd85
Evgen_cv <evgen_cv@mail.ru>
2020-01-29 23:53:04 +0300

    requestPermissions WRITE_EXTERNAL_STORAGE

--- src/main/java/com/kopeyka/android/photoreport/NoteCameraActivity.java
+++ src/main/java/com/kopeyka/android/photoreport/NoteCameraActivity.java
@@ -16,15 +16,20 @@ public class NoteCameraActivity extends SingleFragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         if(ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            super.onCreate(savedInstanceState);
-        }else{
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA}, 0);
         }
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+            super.onCreate(savedInstanceState);
 
 
 

bbc0d90d3efebe0322fe750a0c6aa049dafb182b
Evgen_cv <evgen_cv@mail.ru>
2020-04-16 01:00:22 +0300

    add splash screen add about/instruction activity

--- src/main/java/com/kopeyka/android/photoreport/AboutActivity.java
+++ src/main/java/com/kopeyka/android/photoreport/AboutActivity.java
@@ -1,16 +1,9 @@
 package com.kopeyka.android.photoreport;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
 
 
